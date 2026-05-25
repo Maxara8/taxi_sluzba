598a6b42c792d3c563caa748e7c010e060cec160
Maxara8 <strahinjamaksimovic05@gmail.com>
2026-05-26 00:42:41 +0200

    Adjusting files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
